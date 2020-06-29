dc5c15b9a65042e371fb2a8d83caf70ed27ec0b4
loncarBogdan <bogdan.loncar@bluecouchdigital.com>
2020-06-29 08:50:57 +0200

    initial comint, rename files based on parent folders names

---
+++ TvShowRenaming/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace TvShowRenaming
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSelectedFolder = new System.Windows.Forms.TextBox();
+            this.btnSelectFolder = new System.Windows.Forms.Button();
+            this.btnRename = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.lblLog = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter root folder";
+            // 
+            // txtSelectedFolder
+            // 
+            this.txtSelectedFolder.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSelectedFolder.Location = new System.Drawing.Point(128, 36);
+            this.txtSelectedFolder.Name = "txtSelectedFolder";
+            this.txtSelectedFolder.Size = new System.Drawing.Size(310, 20);
+            this.txtSelectedFolder.TabIndex = 1;
+            // 
+            // btnSelectFolder
+            // 
+            this.btnSelectFolder.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSelectFolder.Location = new System.Drawing.Point(469, 35);
+            this.btnSelectFolder.Name = "btnSelectFolder";
+            this.btnSelectFolder.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectFolder.TabIndex = 2;
+            this.btnSelectFolder.Text = "Select folder";
+            this.btnSelectFolder.UseVisualStyleBackColor = true;
+            this.btnSelectFolder.Click += new System.EventHandler(this.btnSelectFolder_Click);
+            // 
+            // btnRename
+            // 
+            this.btnRename.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRename.Location = new System.Drawing.Point(570, 36);
+            this.btnRename.Name = "btnRename";
+            this.btnRename.Size = new System.Drawing.Size(75, 23);
+            this.btnRename.TabIndex = 3;
+            this.btnRename.Text = "Rename";
+            this.btnRename.UseVisualStyleBackColor = true;
+            this.btnRename.Click += new System.EventHandler(this.btnRename_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(298, 69);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(78, 13);
+            this.lblResult.TabIndex = 4;
+            this.lblResult.Text = "Waiting input...";
+            // 
+            // txtLog
+            // 
+            this.txtLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtLog.Location = new System.Drawing.Point(42, 127);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(603, 118);
+            this.txtLog.TabIndex = 5;
+            // 
+            // lblLog
+            // 
+            this.lblLog.AutoSize = true;
+            this.lblLog.Location = new System.Drawing.Point(39, 111);
+            this.lblLog.Name = "lblLog";
+            this.lblLog.Size = new System.Drawing.Size(25, 13);
+            this.lblLog.TabIndex = 6;
+            this.lblLog.Text = "Log";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(691, 257);
+            this.Controls.Add(this.lblLog);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnRename);
+            this.Controls.Add(this.btnSelectFolder);
+            this.Controls.Add(this.txtSelectedFolder);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSelectedFolder;
+        private System.Windows.Forms.Button btnSelectFolder;
+        private System.Windows.Forms.Button btnRename;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.Label lblLog;
+    }
+}
+
---
+++ TvShowRenaming/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TvShowRenaming
+{
+    public partial class Form1 : Form
+    {
+        private List<string> videoExtensions = new List<string>()
+        {
+            ".avi",
+            ".mp4",
+            ".mkv"
+        };
+
+        private List<string> subtitleExtensions = new List<string>()
+        {
+            ".srt",
+            ".sub"
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelectFolder_Click(object sender, EventArgs e)
+        {
+            var selectFolderDialog = new FolderBrowserDialog();
+            if (selectFolderDialog.ShowDialog() == DialogResult.OK)
+            {
+                txtSelectedFolder.Text = selectFolderDialog.SelectedPath;
+            }
+        }
+
+        private void btnRename_Click(object sender, EventArgs e)
+        {
+            lblResult.Text = "Working...";
+            var rootDirectory = new DirectoryInfo(txtSelectedFolder.Text);
+            if (rootDirectory.Exists)
+            {
+                RenameFilesChildInDir(rootDirectory, rootDirectory.Name);
+            }
+            lblResult.Text = "Finished...";
+        }
+
+        private void RenameFilesChildInDir(DirectoryInfo dir, string prefix)
+        {
+            FindFilesAndRenameInFolder(dir, prefix);
+
+            var choldDirs = dir.GetDirectories().ToList();
+            foreach (var childDir in choldDirs)
+            {
+                RenameFilesChildInDir(childDir, $"{prefix}_{childDir.Name}");
+            }
+        }
+
+        private void FindFilesAndRenameInFolder(DirectoryInfo dir, string prefix)
+        {
+            var filesInDir = dir.GetFiles().ToList();
+            var videoFile = filesInDir.FirstOrDefault(x => videoExtensions.Contains(x.Extension));
+            if (videoFile != null)
+            {
+                var newVideoFilePath = Path.Combine(videoFile.DirectoryName, $"{prefix}{videoFile.Extension}");//GetFileNameWithPrefixWithExtension(videoFile, prefix);
+                MoveFile(videoFile.FullName, newVideoFilePath);
+                videoFile = new FileInfo(newVideoFilePath);
+                var subtitleFile = filesInDir.FirstOrDefault(x => subtitleExtensions.Contains(x.Extension));
+                if (subtitleFile != null)
+                {
+                    MoveFile(subtitleFile.FullName, $"{GetFileNameWithoutExtension(videoFile)}{subtitleFile.Extension}");
+                }
+            }
+        }
+
+        private void MoveFile(string oldFullName, string newFullName)
+        {
+            try
+            {
+                File.Move(oldFullName, newFullName);
+            }
+            catch (Exception ex)
+            {
+                txtLog.Text += $@"
+error on old file: {oldFullName}, new file: {newFullName}.
+error: {ex.Message}";
+            }
+        }
+
+        private string GetFileNameWithoutExtension(FileInfo file)
+        {
+            return file.FullName.Substring(0, file.FullName.Length - file.Extension.Length);
+        }
+    }
+}
